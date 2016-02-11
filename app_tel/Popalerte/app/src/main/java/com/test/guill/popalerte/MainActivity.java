2becaebfdea62d0c3b2aa292301eb720fc9e58d1
Yorokobii <lennonaddicts@hotmail.fr>
2016-02-11 21:44:21 +0100

    modifs surtout sur la version salle

--- app_tel/Popalerte/app/src/main/java/com/test/guill/popalerte/MainActivity.java
+++ app_tel/Popalerte/app/src/main/java/com/test/guill/popalerte/MainActivity.java
@@ -1,5 +1,6 @@
 package com.test.guill.popalerte;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,22 +8,215 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.os.Vibrator;
 import android.util.Log;
-import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+
+class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_ALERTES = "comments";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+
+    private static final String DATABASE_NAME = "alertes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Commande sql pour la création de la base de données
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_ALERTES + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_NAME
+            + " text not null);";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        Log.i("MainActivity", "MySQLiteHelper");
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALERTES);
+        onCreate(db);
+    }
+}
+
+class Alerte {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Sera utilisée par ArrayAdapter dans la ListView
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
+class CommentsDataSource {
+
+    // Champs de la base de données
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_NAME};
+
+    public CommentsDataSource(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public Alerte createAlerte(String name) {
+        ContentValues values = new ContentValues();
+        values.put(MySQLiteHelper.COLUMN_NAME, name);
+        long insertId = database.insert(MySQLiteHelper.TABLE_ALERTES, null,
+                values);
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_ALERTES,
+                allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+        cursor.moveToFirst();
+        Alerte newAlerte = cursorToAlerte(cursor);
+        cursor.close();
+        return newAlerte;
+    }
+
+    public void deleteAlerte(Alerte alerte) {
+        long id = alerte.getId();
+        System.out.println("Comment deleted with id: " + id);
+        database.delete(MySQLiteHelper.TABLE_ALERTES, MySQLiteHelper.COLUMN_ID
+                + " = " + id, null);
+    }
+
+    public List<Alerte> getAllAlertes() {
+        List<Alerte> alertes = new ArrayList<Alerte>();
+
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_ALERTES,
+                allColumns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Alerte alerte= cursorToAlerte(cursor);
+            alertes.add(alerte);
+            cursor.moveToNext();
+        }
+        // assurez-vous de la fermeture du curseur
+        cursor.close();
+        return alertes;
+    }
+
+    private Alerte cursorToAlerte(Cursor cursor) {
+        Alerte alerte= new Alerte();
+        alerte.setId(cursor.getLong(0));
+        alerte.setName(cursor.getString(1));
+        return alerte;
+    }
+}
+
+
+class TestDatabaseActivity extends ListActivity {
+    public CommentsDataSource datasource;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i("MainActivity", "TestDatabaseActivity");
+        super.onCreate(savedInstanceState);
+
+        datasource = new CommentsDataSource(this);
+        datasource.open();
+
+        List<Alerte> values = datasource.getAllAlertes();
+
+        // utilisez SimpleCursorAdapter pour afficher les
+        // éléments dans une ListView
+        ArrayAdapter<Alerte> adapter = new ArrayAdapter<Alerte>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    // Sera appelée par l'attribut onClick
+    // des boutons déclarés dans main.xml
+    public void onClick(View view) {
+        Log.i("MainActivity", "onClick");
+        ArrayAdapter<Alerte> adapter = (ArrayAdapter<Alerte>) getListAdapter();
+        Alerte alerte= null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] alertes = new String[] { "feu", "innondations", "accident" };
+                int nextInt = new Random().nextInt(3);
+                // enregistrer le nouveau commentaire dans la base de données
+                alerte = datasource.createAlerte(alertes[nextInt]);
+                //adapter.add(alerte);
+                break;
+            case R.id.delete:
+                Log.i("MainActivity", String.valueOf(getListAdapter().getCount()));
+                /*if (getListAdapter().getCount() > 0) {
+                    alerte = (Alerte) getListAdapter().getItem(0);
+                    datasource.deleteAlerte(alerte);
+                    adapter.remove(alerte);
+                }*/
+                break;
+        }
+        //adapter.notifyDataSetChanged();
+    }
+}
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     private boolean alerte = true;
+    TestDatabaseActivity database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //cette fonction définie le comportement de l'application a son démarrage
         super.onCreate(savedInstanceState);
+        database = new TestDatabaseActivity();
         setContentView(R.layout.activity_main); //la page par défaut est la page d'accueil
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        new TestDatabaseActivity();
 
         /*************************************
          * comportement du TabHost
@@ -195,14 +389,20 @@ public class MainActivity extends AppCompatActivity {
         image_type.setImageResource(R.drawable.type_incendie);
     }
 
+    public void onClick(View view){
+        database.onClick(view);
+    }
+
     @Override
     protected void onResume() { //fonction appelé quand on reviens sur l'application apres avoir tapé sur le boutton "home"
+        database = new TestDatabaseActivity();
         super.onResume();
     }
 
     @Override
     protected void onPause() { //fonction appelé aprés avoir tapé sur le boutton "home"
         super.onPause();
+        database.datasource.close();
         findViewById(R.id.tabHost).setVisibility(View.VISIBLE); //affiche l'accueil
         findViewById(R.id.Indications).setVisibility(View.GONE); //cache le layout des indications
         // afin d'éviter que le thread de la boussole continue en arriere plan
@@ -231,3 +431,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
