69872231429b4aaa662596c613164931597ab6da
Guillaume Boehm <Guillaume Boehm>
2016-03-17 18:13:06 +0100

    grosses modifs pour la boussole tout est dans la classe Indication, encore quelques modifications possible pour améliorer la précision de la boussole

--- ptut/PopAlerte/app/src/main/java/ptut_sdis30/popalerte/Indications.java
+++ ptut/PopAlerte/app/src/main/java/ptut_sdis30/popalerte/Indications.java
@@ -1,33 +1,41 @@
 package ptut_sdis30.popalerte;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
+import java.lang.Math;
 
-public class Indications extends AppCompatActivity implements SensorEventListener {
-    //Thread thread_boussole;
+public class Indications extends AppCompatActivity implements SensorEventListener, LocationListener {
 
-    private ImageView mPointer;
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private Sensor mMagnetometer;
-    private float[] mLastAccelerometer = new float[3];
-    private float[] mLastMagnetometer = new float[3];
-    private boolean mLastAccelerometerSet = false;
-    private boolean mLastMagnetometerSet = false;
-    private float[] mR = new float[9];
-    private float[] mOrientation = new float[3];
-    private float mCurrentDegree = 0f;
+    TextView text;
+
+    private double Clong = 10.0d;
+    private double Clat = 60.0d;
+    private float rayon_alerte = 10.0f; //rayon de l'alerte en metres
+
+    private double Nordlong = Clong;
+    private double Nordlat = 90.0d;
+
+    private double angle = 0.0d;
+    private float distance_sortie = 0.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,11 @@ public class Indications extends AppCompatActivity implements SensorEventListene
         setContentView(R.layout.activity_indications);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getLocation();
+
+        text = (TextView) findViewById(R.id.textDistance);
+        setPosition();
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -54,8 +67,73 @@ public class Indications extends AppCompatActivity implements SensorEventListene
         mSensorManager.unregisterListener(this, mMagnetometer);
     }
 
+    public void setPosition(){
+        if(this.canGetLocation ){
+            this.getLocation();
+            setAngle(this.getLatitude(), this.getLongitude());
+            updateText();
+        }else{
+            text.setText("UnableToFind");
+            System.out.println("Unable");
+        }
+    }
+
+    public void updateText(){
+        float VecY = (float)(this.getLatitude() - Clat);
+        float VecX = (float)(this.getLongitude() - Clong);
+        VecX = (VecX * 111000.32f); //111.32 km
+        VecY = (VecY * 111000.32f); //111.32 km
+
+        float distCentre = (float)Math.sqrt((float)(Math.pow(VecX, 2.0f) + Math.pow(VecY, 2.0f)));
+        distance_sortie = distCentre;
+
+        text.setText("Distance : " + distance_sortie + " mètres");
+    }
+
+    public void setAngle(double Lat, double Long){
+        Clong = 4.62d;
+        Clat = 43.66d;
+
+        double ABx = Nordlong - Clong;
+        double ABy = Nordlat - Clat;
+
+        double ACx = Long - Clong;
+        double ACy = Lat - Clat;
+
+        angle = Math.toDegrees( Math.atan2(ABx*ACy - ABy*ACx, ABx*ACx + ABy*ACy) );
+        Log.i("Longitude", Double.toString(Long));
+        Log.i("Latitude", Double.toString(Lat));
+    }
+
+    public void Retour(View view){
+        Intent i = new Intent(this, Accueil.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+
+    /*****************************************************************************************
+     * implementation de SensorEventListener
+     */
+    private ImageView mPointer;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
+
     @Override
     public void onSensorChanged(SensorEvent event) {
+
         if (event.sensor == mAccelerometer) {
             System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
             mLastAccelerometerSet = true;
@@ -68,6 +146,7 @@ public class Indications extends AppCompatActivity implements SensorEventListene
             SensorManager.getOrientation(mR, mOrientation);
             float azimuthInRadians = mOrientation[0];
             float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            azimuthInDegress = azimuthInDegress + (float)angle;
             RotateAnimation ra = new RotateAnimation(
                     mCurrentDegree,
                     -azimuthInDegress,
@@ -90,54 +169,175 @@ public class Indications extends AppCompatActivity implements SensorEventListene
 
     }
 
-        /*final ImageView image_boussole;
-        image_boussole = (ImageView) findViewById(R.id.boussole);//crée et récupère l'image de la boussole
+    /********************************************************************************************
+     * implementation de LocationListener
+     */
 
-        image_boussole.setRotationX(image_boussole.getDrawable().getBounds().width() / 2); //définie le point de rotation de l'image
-        image_boussole.setRotationY(image_boussole.getDrawable().getBounds().height() / 2);//de la boussole au centre de l'image
+    // flag for GPS status
+    public boolean isGPSEnabled = false;
 
-        Runnable r = new Runnable() {
-            float rot = 0;
-            @Override
-            public void run() {
-                /**************
-                 * boucle while si la page d'indications est affiché
-                 ***********//*
-                try {
-                    Thread.sleep(500); // attends pendant 10 millisecondes avant de refaire un tour de boucle
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                while (true) {
-                    try {
-                        Thread.sleep(10); // attends pendant 10 millisecondes avant de refaire un tour de boucle
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    image_boussole.post(new Runnable() { //fonction de l'image pour la faire tourner
-                        @Override
-                        public void run() {
-                            rot = rot + 1; //augmente les degrés de rotation de 1 pour fair tourner la boussole
-                            image_boussole.setRotation(rot); //fait tourner l'image de "rot" degrés depuis la premiere position
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    // flag if status is ok
+    boolean canGetLocation = false;
+
+    Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1; // 1 minute
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    /**
+     * Function to get the user's current location
+     *
+     * @return
+     */
+    public Location getLocation() {
+        try {
+
+            if ( Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                throw new Error("permissions not grandeed");
+            }
+
+            locationManager = (LocationManager) this
+                    .getSystemService(Context.LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            Log.v("isGPSEnabled", "=" + isGPSEnabled);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
+
+            if (isGPSEnabled == false && isNetworkEnabled == false) {
+                throw new Error("no Network nor GPS");
+            } else {
+                this.canGetLocation = true;
+                // if GPS Enabled get lat/long using GPS Services
+                if (isNetworkEnabled) {
+                    location=null;
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
                         }
-                    });
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    location=null;
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
                 }
             }
-        };
-        thread_boussole = new Thread(r);
-        thread_boussole.start();
 
-    }*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    public void Retour(View view){
-        Intent i = new Intent(this, Accueil.class);
-        startActivity(i);
-        //thread_boussole.interrupt();
+        return location;
+    }
+
+    /**
+     * Stop using GPS listener Calling this function will stop using GPS in your
+     * app
+     * */
+    public void stopUsingGPS(Context context) {
+        if (locationManager != null) {
+            if ( Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                throw new Error("Permissions not grandeed");
+            }
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+    /**
+     * Function to check GPS/wifi enabled
+     *
+     * @return boolean
+     * */
+    public boolean canGetLocation() {
+        return this.canGetLocation;
     }
 
     @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
+    public void onLocationChanged(Location location) {
+        this.getLocation();
+        setPosition();
     }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
 
 }
